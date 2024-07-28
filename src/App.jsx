eb2e6ede78e6cab233365259a3b299b245a6902d
Gekyume <contatogekyume@gmail.com>
2024-07-27 23:55:58 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export function App() {
 
   return (
     <>
-      
+      <Login/>
     </>
   )
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,45 @@
+// import "./Login.css";
+
+// export function Login() {
+//   return (
+//     <section>
+//       <div className="div-login">
+//         <h1>Gerencia<span>Vagas</span></h1>
+
+//         <form action="">
+//           <label htmlFor="">Nome ou E-Mail</label>
+//           <input type="text" />
+
+//           <label htmlFor="">Senha</label>
+//           <input type="text" />
+//         </form>
+//       </div>
+//     </section>
+//   );
+// }
+
+import "./Login.css";
+
 export function Login() {
   return (
     <section>
-      <div></div>
+      <div className="div-login">
+        <h1>Gerencia<span>Vagas</span></h1>
 
-      <div></div>
+        <form action="">
+          <div className="form-group">
+            <label htmlFor="username">Nome ou E-Mail</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" />
+          </div>
+
+          <input type="submit" value="Fazer Login"/>
+        </form>
+      </div>
     </section>
   );
 }
+
